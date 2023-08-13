af8e2abc3b580d43452e60db7465db5a66288526
singh-kaustubh <kaustubhsingh38192@gmail.com>
2023-08-13 21:01:25 +0530

    restauant screen fixes

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text, ScrollView, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
+import { urlFor } from '../sanity';
+import { ArrowLeftIcon } from 'react-native-heroicons/outline';
 
 const RestaurantScreen = () => {
     const navigation = useNavigation();
@@ -24,9 +26,24 @@ const RestaurantScreen = () => {
         })
     })
     return (
-        <View>
-            <Text>RestaurantScreen</Text>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View className="relative">
+                <Image
+                    source={{
+                        uri: urlFor(imgUrl).url()
+                    }}
+                    className="w-full h-56 p-4 bg-gray-50"
+                ></Image>
+                <TouchableOpacity onPress={navigation.goBack} className="absolute top-11 left-4 p-2 bg-gray-200 rounded-full">
+                    <ArrowLeftIcon size={20} color={"#00CCBB"} />
+                </TouchableOpacity>
+            </View>
+            <View className="bg-white">
+                <View className="px-4 pt-4">
+                    <Text className="text-3xl font-bold">{title}</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
